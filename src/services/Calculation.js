51b127fde5037d732622b17947a4aa5012253453
AtsushiNi <atsushi9731@gmail.com>
2020-05-10 19:52:53 +0900

    変数名リファクタ

--- src/services/Calculation.js
+++ src/services/Calculation.js
@@ -4,20 +4,20 @@ class Calculation {
     // g
     //
     // 大きさ(不変)
-    // boxConfigs[i].size
+    // boxConfigs[index].size
     //
     // 位置[x, y, z]
-    // boxes[i].position
+    // boxes[index].position
     // 向き[x, y, z]
-    // boxes[i].lotation
+    // boxes[index].lotation
     // 速度[x, y, z]
-    // boxes[i].velocity
+    // boxes[index].velocity
     // 角速度[x, y, z]
-    // boxes[i].lotVelocity
+    // boxes[index].lotVelocity
     //
-    boxes.forEach((box, i) => {
+    boxes.forEach((box, index) => {
       // 速度の更新
-      if (boxConfigs[i].fixed === false) {
+      if (boxConfigs[index].fixed === false) {
         box.velocity[0] += gravity[0]
         box.velocity[1] += gravity[1]
         box.velocity[2] += gravity[2]
@@ -34,19 +34,19 @@ class Calculation {
   }
 
   static resetValues = (boxes, boxConfigs) => {
-    boxes.forEach((box, i) => {
-      box.position[0] = boxConfigs[i].initialPosition[0]
-      box.position[1] = boxConfigs[i].initialPosition[1]
-      box.position[2] = boxConfigs[i].initialPosition[2]
-      box.lotation[0] = boxConfigs[i].initialLotation[0]
-      box.lotation[1] = boxConfigs[i].initialLotation[1]
-      box.lotation[2] = boxConfigs[i].initialLotation[2]
-      box.velocity[0] = boxConfigs[i].initialVelocity[0]
-      box.velocity[1] = boxConfigs[i].initialVelocity[1]
-      box.velocity[2] = boxConfigs[i].initialVelocity[2]
-      box.lotVelocity[0] = boxConfigs[i].initialLotVelocity[0]
-      box.lotVelocity[1] = boxConfigs[i].initialLotVelocity[1]
-      box.lotVelocity[2] = boxConfigs[i].initialLotVelocity[2]
+    boxes.forEach((box, index) => {
+      box.position[0] = boxConfigs[index].initialPosition[0]
+      box.position[1] = boxConfigs[index].initialPosition[1]
+      box.position[2] = boxConfigs[index].initialPosition[2]
+      box.lotation[0] = boxConfigs[index].initialLotation[0]
+      box.lotation[1] = boxConfigs[index].initialLotation[1]
+      box.lotation[2] = boxConfigs[index].initialLotation[2]
+      box.velocity[0] = boxConfigs[index].initialVelocity[0]
+      box.velocity[1] = boxConfigs[index].initialVelocity[1]
+      box.velocity[2] = boxConfigs[index].initialVelocity[2]
+      box.lotVelocity[0] = boxConfigs[index].initialLotVelocity[0]
+      box.lotVelocity[1] = boxConfigs[index].initialLotVelocity[1]
+      box.lotVelocity[2] = boxConfigs[index].initialLotVelocity[2]
     })
   }
 }
