0e197c51fd490441b3ac05b2430f12787fd76234
Takamaru <45711951+mizumoti@users.noreply.github.com>
2020-05-11 11:19:50 +0900

    クォータニオンの更新

--- src/models/Box.js
+++ src/models/Box.js
@@ -12,14 +12,21 @@ export default class Box {
     lotVelocityX = 0,
     lotVelocityY = 0,
     lotVelocityZ = 0,
-    quatenionX =0,
-    quatenionY =0,
-    quatenionZ =0,
+    quaternionW =0,
+    quaternionX =0,
+    quaternionY =0,
+    quaternionZ =0,
+    quatVelocityW = 0,
+    quatVelocityX = 0,
+    quatVelocityY = 0,
+    quatVelocityZ = 0,
+    
   ) {
     this.position = [positionX, positionY, positionZ]
     this.lotation = [lotationX, lotationY, lotationZ]
     this.velocity = [velocityX, velocityY, velocityZ]
     this.lotVelocity = [lotVelocityX, lotVelocityY, lotVelocityZ]
-    this.quatenion = [quatenionX, quatenionY, quatenionZ]
+    this.quaternion = [quaternionW, quaternionX, quaternionY, quaternionZ]
+    this.quatVelocity = [quatVelocityW, quatVelocityX, quatVelocityY, quatVelocityZ]
   }
 }
--- src/services/Calculation.js
+++ src/services/Calculation.js
@@ -1,78 +1,87 @@
 class Calculation {
 
-  matrix_cl(mat_A, mat_B){
-    const n_mat = [[0,0,0],
+  matrix_cl(matrixA, matrixB){
+    const n_matrix = [[0,0,0],
                  [0,0,0],
-                 [0,0,0]];
+                 [0,0,0]]
     for(let i=0; i<3; i++){
       for(let j=0; j<3; j++){
         for(let k=0; k<3; k++){
-          n_mat[i][j] = mat_A[i][k] * mat_B[k][j];
+          n_matrix[i][j] = matrixA[i][k] * matrixB[k][j]
         }
       }
     }
-   return n_mat; 
+   return n_matrix 
   }
 
   //内因性オイラー角(x→y→z)から回転行列への変換
-  rot_mat_cnv(θx,θy,θz){
-    const rot_mat = [[Math.cos(θy)*Math.cos(θz), Math.sin(θx)*Math.sin(θy)*Math.cos(θz)-Math.cos(θx)*Math.sin(θz), Math.cos(θx)*Math.sin(θy)*Math.cos(θz)+Math.sin(θx)*Math.sin(θz)],
-                     [Math.cos(θy)*Math.sin(θz), Math.sin(θx)*Math.sin(θy)*Math.sin(θz)+Math.cos(θx)*Math.cos(θz), Math.cos(θx)*Math.sin(θy)*Math.sin(θz)-Math.sin(θx)*Math.cos(θz)],
-                     [-Math.sin(θy), Math.sin(θx)*Math.cos(θy), Math.cos(θx)*Math.cos(θy)]];
-    return rot_mat;
+  rot_mat_cnv(lotx,loty,lotz){
+    const rot_matrix = [[Math.cos(loty)*Math.cos(lotz), Math.sin(lotx)*Math.sin(loty)*Math.cos(lotz)-Math.cos(lotx)*Math.sin(lotz), Math.cos(lotx)*Math.sin(loty)*Math.cos(lotz)+Math.sin(lotx)*Math.sin(lotz)],
+                     [Math.cos(loty)*Math.sin(lotz), Math.sin(lotx)*Math.sin(loty)*Math.sin(lotz)+Math.cos(lotx)*Math.cos(lotz), Math.cos(lotx)*Math.sin(loty)*Math.sin(lotz)-Math.sin(lotx)*Math.cos(lotz)],
+                     [-Math.sin(loty), Math.sin(lotx)*Math.cos(loty), Math.cos(lotx)*Math.cos(loty)]]
+    return rot_matrix
   }
 
   //回転行列からクォータニオンへの変換
-  quat_cnv(rot_mat){
-    const quat_vct = [0, 0, 0, 0]; 
-    let w = Math.sqrt(rot_mat[1][1]+rot_mat[2][2]+rot_mat[3][3]+1)/2;
-    let x = Math.sqrt(rot_mat[1][1]-rot_mat[2][2]-rot_mat[3][3]+1)/2;
-    let y = Math.sqrt(-rot_mat[1][1]+rot_mat[2][2]-rot_mat[3][3]+1)/2;
-    let z = Math.sqrt(-rot_mat[1][1]-rot_mat[2][2]+rot_mat[3][3]+1)/2;
-    const max_quat = Math.max(w, x, y, z);
+  quat_cnv(rot_matrix){
+    const quat_vector = [0, 0, 0, 0]
+    let w = Math.sqrt(rot_matrix[0][0]+rot_matrix[1][1]+rot_matrix[2][2]+1)/2
+    let x = Math.sqrt(rot_matrix[0][0]-rot_matrix[1][1]-rot_matrix[2][2]+1)/2
+    let y = Math.sqrt(-rot_matrix[0][0]+rot_matrix[1][1]-rot_matrix[2][2]+1)/2
+    let z = Math.sqrt(-rot_matrix[0][0]-rot_matrix[1][1]+rot_matrix[2][2]+1)/2
+    const max_quat = Math.max(w, x, y, z)
 
     if(max_quat === x){
-      y = (rot_mat[1][2]+rot_mat[2][1])/(4 * x);
-      z = (rot_mat[3][1]+rot_mat[1][3])/(4 * x);
+      y = (rot_matrix[0][1]+rot_matrix[1][0])/(4 * x)
+      z = (rot_matrix[2][0]+rot_matrix[0][2])/(4 * x)
+      w = (rot_matrix[1][2]-rot_matrix[2][1])/(4 * x)
     }else if(max_quat === y){
-      z = (rot_mat[2][3]+rot_mat[3][2])/(4 * y);
-      x = (rot_mat[1][2]+rot_mat[2][1])/(4 * y);
+      z = (rot_matrix[1][2]+rot_matrix[2][1])/(4 * y)
+      x = (rot_matrix[0][1]+rot_matrix[1][0])/(4 * y)
+      w = (rot_matrix[2][0]-rot_matrix[0][2])/(4 * y)
     }else if(max_quat === z){
-      x = (rot_mat[3][1]+rot_mat[1][3])/(4 * z);
-      y = (rot_mat[2][3]+rot_mat[3][2])/(4 * z);
+      x = (rot_matrix[2][0]+rot_matrix[0][2])/(4 * z)
+      y = (rot_matrix[1][2]+rot_matrix[2][1])/(4 * z)
+      w = (rot_matrix[0][1]-rot_matrix[1][0])/(4 * z)
+    }else{
+      x = (rot_matrix[1][2]-rot_matrix[2][1])/(4 * w)
+      y = (rot_matrix[2][0]-rot_matrix[0][2])/(4 * w)
+      z = (rot_matrix[0][1]-rot_matrix[1][0])/(4 * w)
     }
     
-    quat_vct[0] = w;
-    quat_vct[1] = x; 
-    quat_vct[2] = y;
-    quat_vct[3] = z;
+    quat_vector[0] = w
+    quat_vector[1] = x 
+    quat_vector[2] = y
+    quat_vector[3] = z
 
-    return quat_vct;
+    return quat_vector
   }
 
   //クォータニオンの計算:q=[w,x,y,z]
   quat_cl(q, p){
-    const n_quat_vct = [0,0,0,0];
-    n_quat_vct[0] = q[0]*p[0] - q[1]*p[1] - q[2]*p[2] - q[3]*p[3];
-    n_quat_vct[1] = q[1]*p[0] + q[0]*p[1] - q[3]*p[2] + q[2]*p[3];
-    n_quat_vct[2] = q[2]*p[0] + q[3]*p[1] - q[0]*p[2] - q[1]*p[3];
-    n_quat_vct[3] = q[3]*p[0] - q[2]*p[1] + q[1]*p[2] - q[0]*p[3];
-    return n_quat_vct;
+    const n_quat_vector = [0,0,0,0]
+    n_quat_vector[0] = q[0]*p[0] - q[1]*p[1] - q[2]*p[2] - q[3]*p[3]
+    n_quat_vector[1] = q[1]*p[0] + q[0]*p[1] - q[3]*p[2] + q[2]*p[3]
+    n_quat_vector[2] = q[2]*p[0] + q[3]*p[1] - q[0]*p[2] - q[1]*p[3]
+    n_quat_vector[3] = q[3]*p[0] - q[2]*p[1] + q[1]*p[2] - q[0]*p[3]
+    return n_quat_vector
   }
 
   //位置ベクトルをクォータニオンに変換
   quat_r(r){
-    const r_quat = [0, r[0], r[1], r[2]];
-    return r_quat;
+    const r_quat = [0, r[0], r[1], r[2]]
+    return r_quat
   }
   //各速度ベクトルをクォータニオンに変換
   quat_w(w){
-    const w_quat = [1, w[0], w[1], w[2]];
-    return w_quat;
+    const quatVelocity = [1, w[0], w[1], w[2]]
+    return quatVelocity
   }
   //各速度ベクトルω=[ω1,ω2,ω3]のときのクォータニオンの更新
-  quat_update(q, w){
-    q += this.quat_cl(q, w) - q;
+  quat_update(quaternion, w){
+    for(let i=0; i<4; i++)
+      quaternion[i] += this.quat_cl(quaternion, w)[i] - quaternion[i]
+    return quaternion
   }
 
 
@@ -107,6 +116,8 @@ class Calculation {
       box.lotation[0] += box.lotVelocity[0]
       box.lotation[1] += box.lotVelocity[1]
       box.lotation[2] += box.lotVelocity[2]
+      //クォータニオンの更新
+      box.quaternion.quat_update(box.quaternion, box.quatVelocity)
     })
   }
 
