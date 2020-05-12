0cd46ac90d7267cd7d9fe1b6759986a4fa65ac3a
AtsushiNi <atsushi9731@gmail.com>
2020-05-12 23:11:03 +0900

    modelの初期値をベタがき

--- src/models/Box.js
+++ src/models/Box.js
@@ -1,32 +1,10 @@
 export default class Box {
-  constructor(
-    positionX = 0,
-    positionY = 0,
-    positionZ = 0,
-    lotationX = 0,
-    lotationY = 0,
-    lotationZ = 0,
-    velocityX = 0,
-    velocityY = 0,
-    velocityZ = 0,
-    lotVelocityX = 0,
-    lotVelocityY = 0,
-    lotVelocityZ = 0,
-    quaternionW =0,
-    quaternionX =0,
-    quaternionY =0,
-    quaternionZ =0,
-    quatVelocityW = 0,
-    quatVelocityX = 0,
-    quatVelocityY = 0,
-    quatVelocityZ = 0,
-    
-  ) {
-    this.position = [positionX, positionY, positionZ]
-    this.lotation = [lotationX, lotationY, lotationZ]
-    this.velocity = [velocityX, velocityY, velocityZ]
-    this.lotVelocity = [lotVelocityX, lotVelocityY, lotVelocityZ]
-    this.quaternion = [quaternionW, quaternionX, quaternionY, quaternionZ]
-    this.quatVelocity = [quatVelocityW, quatVelocityX, quatVelocityY, quatVelocityZ]
+  constructor() {
+    this.position = [0, 0, 0]
+    this.lotation = [0, 0, 0]
+    this.velocity = [0, 0, 0]
+    this.lotVelocity = [0, 0, 0]
+    this.quaternion = [0, 0, 0]
+    this.quatVelocity = [0, 0, 0]
   }
 }
