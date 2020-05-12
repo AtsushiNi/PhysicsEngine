a3fdb65e7d313983a852ec4eb593e50d9c463787
AtsushiNi <atsushi9731@gmail.com>
2020-05-12 22:56:58 +0900

    クォータニオン を用いて描写

--- src/components/objects/Box.jsx
+++ src/components/objects/Box.jsx
@@ -7,12 +7,13 @@ function Box(props) {
 
   useFrame(() => {
     // mesh.current.rotation.x = mesh.current.rotation.y += 0.01
-    mesh.current.rotation.x = props.lotation[0]
-    mesh.current.rotation.y = props.lotation[1]
-    mesh.current.rotation.z = props.lotation[2]
     mesh.current.position.x = props.position[0]
     mesh.current.position.y = props.position[1]
     mesh.current.position.z = props.position[2]
+    mesh.current.quaternion.w = props.quaternion[0]
+    mesh.current.quaternion.x = props.quaternion[1]
+    mesh.current.quaternion.y = props.quaternion[2]
+    mesh.current.quaternion.z = props.quaternion[3]
   })
 
   return (
@@ -24,8 +25,8 @@ function Box(props) {
 }
 
 Box.propTypes = {
-  lotation: PropTypes.arrayOf(PropTypes.number),
   position: PropTypes.arrayOf(PropTypes.number),
+  quaternion: PropTypes.arrayOf(PropTypes.number),
 }
 
 export default Box
