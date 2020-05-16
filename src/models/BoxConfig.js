6a88f04bb6e0f46eb159422e7f7a6860937aede6
AtsushiNi <atsushi9731@gmail.com>
2020-05-16 14:48:33 +0900

    モデルのconstructor簡略化

--- src/models/BoxConfig.js
+++ src/models/BoxConfig.js
@@ -1,31 +1,12 @@
 export default class BoxConfig {
-  constructor(
-    sizeX = 1,
-    sizeY = 1,
-    sizeZ = 1,
-    initialPositionX = 0,
-    initialPositionY = 0,
-    initialPositionZ = 0,
-    initialRotationX = 0,
-    initialRotationY = 0,
-    initialRotationZ = 0,
-    fixed = false
-  ) {
+  constructor(sizeX = 1, sizeY = 1, sizeZ = 1) {
     this.size = [sizeX, sizeY, sizeZ]
-    this.initialPosition = [
-      initialPositionX,
-      initialPositionY,
-      initialPositionZ,
-    ]
-    this.initialRotation = [
-      initialRotationX,
-      initialRotationY,
-      initialRotationZ,
-    ]
+    this.initialPosition = [0, 0, 0]
+    this.initialRotation = [0, 0, 0]
     this.initialVelocity = [0, 0, 0]
     this.standardLotVelocity = 0.03
     this.initialLotVelocity = [0, 0, 0]
     this.standardVelocity = 0.03
-    this.fixed = fixed
+    this.fixed = false
   }
 }
