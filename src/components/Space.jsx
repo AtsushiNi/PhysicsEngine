46c2ba72c248708ce882c60ea8073e0bd1735aa0
AtsushiNi <atsushi9731@gmail.com>
2020-05-28 23:05:54 +0900

    カメラをドラッグで移動できるようにする

--- src/components/Space.jsx
+++ src/components/Space.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import PropTypes from 'prop-types'
-import { Canvas } from 'react-three-fiber'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { Canvas, useThree, useFrame, extend } from 'react-three-fiber'
 import Box from './objects/Box'
 import DebugObject from './objects/DebugObject'
 import { default as BoxInfo } from '../models/Box'
+extend({ OrbitControls })
 
 function Space(props) {
   const boxes = props.boxes.map((box, i) => {
@@ -15,9 +17,8 @@ function Space(props) {
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
       {boxes}
-      {props.boxes.length > 1 &&
-        <DebugObject boxes={props.boxes}/>
-      }
+      {props.boxes.length > 1 && <DebugObject boxes={props.boxes} />}
+      <Controls />
     </Canvas>
   )
 }
@@ -26,4 +27,19 @@ export default Space
 
 Space.propTypes = {
   boxes: PropTypes.arrayOf(PropTypes.instanceOf(BoxInfo)),
+}
+
+function Controls() {
+  const ref = useRef()
+  const { camera, gl } = useThree()
+  useFrame(() => ref.current.update())
+  return (
+    <orbitControls
+      ref={ref}
+      args={[camera, gl.domElement]}
+      enableDamping
+      dampingFactor={0.1}
+      rotateSpeed={0.5}
+    />
+  )
 }
