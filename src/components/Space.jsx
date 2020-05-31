6eb4600982095d7cba208bd979f78388e44e8a2b
AtsushiNi <atsushi9731@gmail.com>
2020-05-31 23:43:42 +0900

    ドラッグで視点を動かせる

--- src/components/Space.jsx
+++ src/components/Space.jsx
@@ -1,25 +1,42 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import PropTypes from 'prop-types'
-import { Canvas } from 'react-three-fiber'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { Canvas, useThree, useFrame, extend } from 'react-three-fiber'
 import Box from './objects/Box'
 import { default as BoxInfo } from '../models/Box'
+extend({ OrbitControls })
 
-function Space(props) {
+export default function Space(props) {
   const boxes = props.boxes.map((box, i) => {
     return <Box box={box} key={i} />
   })
 
   return (
     <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
+      <spotLight intensity={0.4} position={[30, 30, 50]} angle={0.2} penumbra={1} />
+      <spotLight intensity={0.4} position={[-30, 30, 50]} angle={0.2} penumbra={1} />
+      <ambientLight intensity={0.5} />
       {boxes}
+    <Controls />
     </Canvas>
   )
 }
 
-export default Space
-
 Space.propTypes = {
   boxes: PropTypes.arrayOf(PropTypes.instanceOf(BoxInfo)),
+}
+
+function Controls() {
+  const ref = useRef()
+  const { camera, gl } = useThree()
+  useFrame(() => ref.current.update())
+  return (
+    <orbitControls
+      ref={ref}
+      args={[camera, gl.domElement]}
+      enableDamping
+      dampingFactor={0.1}
+      rotateSpeed={0.5}
+    />
+  )
 }
